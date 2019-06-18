c66d29944502cca23b2a3f0d872ba6c17778f16e
kyungji12 <kyungji12@gmail.com>
2019-06-18 19:07:04 +0900

    add toolbar

--- 190617_MyApplication/app/src/main/java/com/example/a190617_myapplication/MainActivity.java
+++ 190617_MyApplication/app/src/main/java/com/example/a190617_myapplication/MainActivity.java
@@ -2,23 +2,33 @@ package com.example.a190617_myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     ImageView login;
     Intent intent;
+    Toolbar tb;
+
+    ActionBar ab = getSupportActionBar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        tb = findViewById(R.id.app_toolbar);
+        setSupportActionBar(tb);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        login = (ImageView)findViewById(R.id.main1_login);
-        login.setOnClickListener(new View.OnClickListener(){
+        login = findViewById(R.id.main1_login);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(getApplicationContext(), Main2_Login.class);
@@ -27,5 +37,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.appbar_action,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_search :
+                ((TextView)findViewById(R.id.textView)).setText(("SEARCH"));
+                return true;
+            case R.id.action_account :
+                ((TextView)findViewById(R.id.textView)).setText(("ACCOUNT"));
+                return true;
+            case R.id.action_settings :
+                ((TextView)findViewById(R.id.textView)).setText(("SETTINGS"));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
